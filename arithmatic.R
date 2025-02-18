c253dc3fd4a392dba97cfa8058c165e975ce8809
Raj-Rathod366 <rajrathod8732@gmail.com>
2025-02-18 08:23:48 +0530

    unit-2 r programing

---
+++ arithmatic.R
@@ -0,0 +1,9 @@
+{
+num1 = as.numeric(readline("Enter number 1: "))
+num2 = as.numeric(readline("Enter number 2: "))
+}
+
+cat("sum:-",num1+num2)
+cat("sub:-",num1-num2)
+cat("divi:-",num1/num2)
+cat("mul :-",num1*num2)
---
+++ assign_value.R
@@ -0,0 +1,11 @@
+a = "raj"
+print(a)
+
+b <- 12
+print(b)
+
+13 -> c
+print(c)
+
+assign("cats", 3)
+print(cats)
